c5bef6677ba08204a9e06258f15abcdb35de7920
Doruk Aytekin <horationelson1903@gmail.com>
2023-10-07 03:10:52 -0700

    automate website

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -1,38 +1,25 @@
 import React from "react";
-import JavaBasic from "../../src/assets/img/certificates/Java Certificate.webp"
-import CS from "../../src/assets/img/certificates/CS-Certificate.webp"
-import CSS from "../../src/assets/img/certificates/CSS Certificate.webp"
-import JSADS from "../../src/assets/img/certificates/JavaScript Algorithms And Data Structures Certificate.webp"
-import JS from "../../src/assets/img/certificates/JavaScript Certificate.webp"
-import PS from "../../src/assets/img/certificates/Problem Solving Certificate.webp"
-import Python from "../../src/assets/img/certificates/Python Certificate.webp"
-import RWD from "../../src/assets/img/certificates/Responsive Web Design Certificate.webp"
-import SQL from "../../src/assets/img/certificates/SQL Certificate.webp"
-import RC from "../../src/assets/img/certificates/React Certificate.webp"
-import FEDC from "../../src/assets/img/certificates/Front-End-Development-Certificate.webp"
-
-
-
+import Sertifikalar from "../certificates.json"
 
 export default class Certificates extends React.Component {
+    certificate = Sertifikalar.certificates.map(sertifika => {
+        return(<Certificate {...sertifika}></Certificate>)
+    })
     render(){
         return(
             <section>
                 <h2 className="certificates-title">My Certifications</h2>
                 <div className="certificates">
-                    <img className="certificate" src={JavaBasic} alt="" />
-                    <img className="certificate" src={CS} alt="" />
-                    <img className="certificate" src={CSS} alt="" />
-                    <img className="certificate" src={JS} alt="" />
-                    <img className="certificate" src={PS} alt="" />
-                    <img className="certificate" src={Python} alt="" />
-                    <img className="certificate" src={RC} alt="" />
-                    <img className="certificate" src={SQL} alt="" />
-                    <img className="certificate" src={RWD} alt="" />
-                    {/* <img className="certificate" src={FEDC} alt="" /> */}
-                    <img className="certificate" src={JSADS} alt="" />
+                    {this.certificate}
                 </div>
             </section>
         );
+    }
+}
+class Certificate extends React.Component{
+    render(){
+        return(
+            <img className="certificate" src={this.props.img} alt="" />
+        )
     }
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,48 +1,34 @@
 import React from "react";
-import MPP from "../../src/assets/img/projects/Personal_Page.webp"
-import Converter from "../../src/assets/img/projects/Convert.webp";
-import CSGO from "../../src/assets/img/projects/Cs Go.webp";
-import MSL from "../../src/assets/img/projects/Library.webp";
-import WWCG from "../../src/assets/img/projects/WW2 Catch Game.webp";
-import AHS from "../../src/assets/img/projects/Amal Hawks Site.webp";
+import Projeler from '../projects.json'
 
-export default class Projects extends React.Component {
+export default class Projects extends React.Component{
+    
+    
+    proje = Projeler.projects.map(project => {              
+        return(<Project {...project}></Project>) 
+    })
     render(){
         return(
+        
             <section id="works">
                 <h2 className="project-title">My Projects</h2>
-                <div className="project-div">
-                    <img className="project-img" src={MPP} alt="doruk aytekin" />
-                    <p className="project-p">Personal Website</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                <div className="project-div">
-                    <img className="project-img" src={Converter} alt="doruk aytekin" />
-                    <p className="project-p">Converter</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                <div className="project-div">
-                    <img className="project-img" src={MSL} alt="doruk aytekin" />
-                    <p className="project-p">My Software Library</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                <div className="project-div">
-                    <img className="project-img" src={WWCG} alt="doruk aytekin" />
-                    <p className="project-p">WW2 Cacth Game</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                <div className="project-div">
-                    <img className="project-img" src={CSGO} alt="doruk aytekin" />
-                    <p className="project-p">Cs Go 2D Editon</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                <div className="project-div">
-                    <img className="project-img" src={AHS} alt="doruk aytekin" />
-                    <p className="project-p">Amal Hawks Website</p>
-                    <a className="project-a" href=""><p>Show More</p></a>
-                </div>
-                
+                {this.proje}
             </section>
+        
+    
+        );    
+    }
+}
+class Project extends React.Component {
+    render(){
+        return(
+            <div className="project-div">
+                <img className="project-img" src={this.props.img} alt=""/>
+                <p className="project-p">{this.props.description}</p>
+                <a className="project-a" href="#"><p>Show More</p></a>
+            </div>
         );
+            
+            
     }
 }
