64ccdcb9602f6b414a6964b54308f951019eca1c
Doruk Aytekin <horationelson1903@gmail.com>
2023-10-28 13:51:04 -0700

    make website to multiple page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import "./App.css"
 function App() {
   return(
-    <div>
+    // make website favoruable for multiple page website 
+    //so you can make sql documentation page and other documentation pages
+    <Router>
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+      </Routes>
       <Footer></Footer>
-    </div>
+    </Router>
+
   );
   
   
