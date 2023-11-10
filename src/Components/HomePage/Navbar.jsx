c09ab7526f202b8f478764f82eaa938b0f737c28
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-10 13:20:34 -0800

    multiple page bug fix

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -5,9 +5,9 @@ export default class Navbar extends React.Component {
            <section className='nav-bar'>
             <h1 className='nav-bar-p'>Doruk Aytekin</h1>
             <ul className='nav-links'>
-                <a href="##home">Home</a>
-                <a href="##works">Work</a>
-                <a href="##about-me">About Me</a>
+                <a href="#home">Home</a>
+                <a href="#works">Work</a>
+                <a href="#about-me">About Me</a>
             </ul>
         </section> 
         );
