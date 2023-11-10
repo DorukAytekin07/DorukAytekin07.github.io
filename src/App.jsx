a121996683512ab48f10b57a4d08ba4f8781df42
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-10 13:07:40 -0800

    multiple page bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import Footer from "./Components/HomePage/Footer"
 import Home from "./Components/Home";
 import SQL from "./Components/SQL";
 import Documents from "./Components/DocumentsPage";
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 import "./App.css"
 import './CSS/Key.css'
 function App() {
   return(
     <HashRouter>
-      <Router>
         <Routes>
           <Route path="/" exact element={<Home/>}/>
           <Route path="https://dorukaytekin07.github.io/sql" exact element={<SQL/>} />
           <Route path="https://dorukaytekin07.github.io/documents" exact element={<Documents/>} />
         </Routes>
         <Footer></Footer>
-      </Router>
     </HashRouter>
     
 
