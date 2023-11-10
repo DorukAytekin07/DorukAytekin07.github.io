faf4f64f47a9017d28986727d460084bcdb6257b
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-10 13:26:39 -0800

    multiple page bug fix

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { HashLink as Link } from 'react-router-hash-link';
 export default class Navbar extends React.Component {
     render(){
         return (
            <section className='nav-bar'>
             <h1 className='nav-bar-p'>Doruk Aytekin</h1>
             <ul className='nav-links'>
-                <a href="#home">Home</a>
-                <a href="#works">Work</a>
-                <a href="#about-me">About Me</a>
+                <Link to="/#home">Home</Link>
+                <Link to="/#works">Work</Link>
+                <Link to="/#about-me">About Me</Link>
             </ul>
         </section> 
         );
