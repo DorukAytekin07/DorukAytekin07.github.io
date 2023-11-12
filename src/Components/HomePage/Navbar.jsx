44de28ae88e1860dd8b1a6894a2431f6eb51a727
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-12 10:33:53 -0800

    improve seo (h1 heading lenght)

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -4,7 +4,7 @@ export default class Navbar extends React.Component {
     render(){
         return (
            <section className='nav-bar'>
-            <h1 className='nav-bar-p'>Doruk Aytekin</h1>
+            <h2 className='nav-bar-p'>Doruk Aytekin</h2>
             <ul className='nav-links'>
                 <Link to="/#home">Home</Link>
                 <Link to="/#works">Work</Link>
