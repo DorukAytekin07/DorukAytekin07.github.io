3ff872e937127b4c64df4cfd69a364a5771202bd
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-12 10:41:44 -0800

    improve seo (to many h1 heading)

--- src/Components/HomePage/Welcome.jsx
+++ src/Components/HomePage/Welcome.jsx
@@ -3,8 +3,8 @@ export default class Welcome extends React.Component {
     render(){
         return(
             <section id="home" className="home-text ">
-                <h1 className="home-text-title">Hello I am Doruk</h1>
-                <h1 className="home-text-desc">a Software Engineer</h1>
+                <h1 className="home-text-title">Hello I am Doruk Aytekin</h1>
+                <h2 className="home-text-desc">a Software Engineer</h2>
             </section>
         );
        
