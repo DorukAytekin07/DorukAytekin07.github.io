3d6b31388f8f584f4dfd45db60bc92a3e2fa7104
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-12 10:28:35 -0800

    improve seo (reduce image size)

--- src/Components/HomePage/Welcome.jsx
+++ src/Components/HomePage/Welcome.jsx
@@ -3,7 +3,7 @@ export default class Welcome extends React.Component {
     render(){
         return(
             <section id="home" className="home-text ">
-                <h2 className="home-text-title">Hello I am Doruk</h2>
+                <h1 className="home-text-title">Hello I am Doruk</h1>
                 <p className="home-text-desc">a Software Engineer</p>
             </section>
         );
