e3a492adc9543bc6bce0082319fa6a6b5fdafd76
Doruk Aytekin <horationelson1903@gmail.com>
2023-11-15 01:00:18 -0800

    write answers of db basics,change text aling for the document and change some text

--- src/Components/Documents/SQL/DBQuestions.jsx
+++ src/Components/Documents/SQL/DBQuestions.jsx
@@ -9,7 +9,7 @@ export default class MySQLQuaries extends React.Component{
         return(
             <div>
                 <h2 className="document-title">Database Basics</h2>
-                <p className="document-summary">In this document my purpose is teaching to you database basics, some SQL queries and MySQL commands if you are ready lets start but before dive into SQL syntax I want to explain some database basics so in this part you will learn basics of database with questions.</p>
+                <p className="document-summary">In this document my purpose is teaching to you database basics, some SQL queries and MySQL commands if you are ready lets start but before dive into SQL syntax and MySQL commands I want to explain some database basics so in this part you will learn basics of database with questions.</p>
                 {this.Question}
             </div>
         );
