930d361f22aa834235fddcb8a633f3ec665a0c8f
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-01-26 15:11:43 -0800

    add pdf of math pages

--- src/Components/Documents/Math/Algebra.jsx
+++ src/Components/Documents/Math/Algebra.jsx
@@ -1,10 +1,18 @@
 import React from "react"
-
+import { Viewer } from '@react-pdf-viewer/core'; // install this library
+import { Worker } from '@react-pdf-viewer/core'; // install this library
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+// const defaultLayoutPluginInstance = defaultLayoutPlugin();
 export default class Algebra extends React.Component{
     render() {
         return(
+            
             <div>
-                 <h1 className="center-middle">Algebra Section</h1>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                    <Viewer fileUrl="./Documents/Math/algebra.pdf"  />
+                </Worker>  
             </div>
         )
     }
--- src/Components/Documents/Math/PreCalculus.jsx
+++ src/Components/Documents/Math/PreCalculus.jsx
@@ -1,10 +1,18 @@
 import React from "react"
-
-export default class PreCalculus extends React.Component{
+import { Viewer } from '@react-pdf-viewer/core'; // install this library
+import { Worker } from '@react-pdf-viewer/core'; // install this library
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+// const defaultLayoutPluginInstance = defaultLayoutPlugin();
+export default class Algebra extends React.Component{
     render() {
         return(
+            
             <div>
-                <h1 className="center-middle">Precalculus Section</h1>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                    <Viewer fileUrl="./Documents/Math/precalculus.pdf"  />
+                </Worker>  
             </div>
         )
     }
