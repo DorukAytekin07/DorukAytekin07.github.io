4ca5fad047f2071cf4ebde28b4e38355e427d6b1
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-01-26 23:21:08 -0800

    change pdf viewer

--- src/Components/Documents/Math/Algebra.jsx
+++ src/Components/Documents/Math/Algebra.jsx
@@ -1,19 +1,18 @@
 import React from "react"
 import { Viewer } from '@react-pdf-viewer/core'; // install this library
-import { Worker } from '@react-pdf-viewer/core'; // install this library
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-// const defaultLayoutPluginInstance = defaultLayoutPlugin();
-export default class Algebra extends React.Component{
-    render() {
-        return(
-            
-            <div>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    <Viewer fileUrl="./Documents/Math/algebra.pdf"  />
-                </Worker>  
-            </div>
-        )
-    }
+import { Worker } from '@react-pdf-viewer/core';
+export const Algebra = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    return(
+        <div className="pdf-container">
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                <Viewer fileUrl={"./Documents/Math/algebra.pdf"}
+                    plugins={[defaultLayoutPluginInstance]} />
+            </Worker>
+        </div>   
+    )
 }
+export default Algebra;
--- src/Components/Documents/Math/PreCalculus.jsx
+++ src/Components/Documents/Math/PreCalculus.jsx
@@ -1,19 +1,18 @@
 import React from "react"
 import { Viewer } from '@react-pdf-viewer/core'; // install this library
-import { Worker } from '@react-pdf-viewer/core'; // install this library
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-// const defaultLayoutPluginInstance = defaultLayoutPlugin();
-export default class Algebra extends React.Component{
-    render() {
-        return(
-            
-            <div>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    <Viewer fileUrl="./Documents/Math/precalculus.pdf"  />
-                </Worker>  
-            </div>
-        )
-    }
+import { Worker } from '@react-pdf-viewer/core';
+export const PreCalculus = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    return(
+        <div className="pdf-container center-middle">
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                <Viewer fileUrl={"./Documents/Math/precalculus.pdf"}
+                    plugins={[defaultLayoutPluginInstance]} />
+            </Worker>
+        </div>   
+    )
 }
+export default PreCalculus;
